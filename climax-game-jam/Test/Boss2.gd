2a8847da207479ca082d7ad155207fb33eee5ee4
Christopher Boczko <chris@furtherthinking.net>
2022-03-05 17:13:59 +0000

    added boss 2 and final boss

---
+++ climax-game-jam/Test/Boss2.gd
@@ -0,0 +1,33 @@
+extends KinematicBody2D
+
+const speed = 1
+var velocity = Vector2()
+
+var is_dead = false
+var scene
+var health = 100;
+
+func _ready():
+	pass
+	
+func hit():
+	PlayerData.boss_health -= 10
+	PlayerData.score += 10
+	if PlayerData.boss_health <= 0:
+		dead();
+		
+func dead():
+	is_dead = true
+	velocity = Vector2(0, 0)
+	scene = "res://climax-game-jam/trans2/trans.tscn"
+	get_tree().change_scene(scene)
+
+func _on_Timer_timeout():
+	queue_free()
+	
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
+
+
+func _on_VisibilityNotifier2D_viewport_entered(viewport):
+	pass # Replace with function body.
