3d91d98c3fb4e003cb4fe945a51820a0a536c710
Christopher Boczko <chris@furtherthinking.net>
2022-03-06 10:17:28 +0000

    gameover screen fied

--- NewGame.gd
+++ NewGame.gd
@@ -6,12 +6,14 @@ func _on_creditsButton_pressed():
 	scene = "res://climax-game-jam/Credits/credits.tscn"
 	$FadeIn.show()
 	$FadeIn.fade_in()
+	get_tree().change_scene(scene)
 
 func _on_StartGameButton_pressed():
 	PlayerData.reset()
 	scene = "res://climax-game-jam/trans1/trans.tscn"
 	$FadeIn.show()
 	$FadeIn.fade_in()
+	get_tree().change_scene(scene)
 	
 func _on_backButton_pressed():
 	scene = "res://climax-game-jam/titlescreen/titlescreen.tscn"
